cbb80ac96b67f0338977d20ab851b6dfd3ac5c7c
Bektur <bekturnurlanov01@gmail.com>
2024-05-17 20:46:37 +0600

    added adaptive styles to ProfilePage

--- src/pages/ProfilePage/ProfilePage.tsx
+++ src/pages/ProfilePage/ProfilePage.tsx
@@ -10,26 +10,28 @@ const ProfilePage: React.FC = () => {
   const recipes = useSelector(
     (state: { recipes: { recipes: IRecipe[] } }) => state.recipes.recipes
   );
+
+  const isSmallScreen = window.innerWidth < 426;
   return (
     <div className={s.profile_page}>
       <h2>Profile</h2>
       <div className={s.user_block}>
         <img src={user} alt="" className={s.user_photo} />
-        <div className={s.user_info}>
-          <div className={s.user_raitings}>
-            <div className={s.user_raiting}>
-              <h2>29</h2>
-              <p>Recipe</p>
-            </div>
-            <div className={s.user_raiting}>
-              <h2>144</h2>
-              <p>Followers</p>
-            </div>
-            <div className={s.user_raiting}>
-              <h2>100</h2>
-              <p>Following</p>
-            </div>
+        <div className={s.user_raitings}>
+          <div className={s.user_raiting}>
+            <h2>29</h2>
+            <p>Recipe</p>
           </div>
+          <div className={s.user_raiting}>
+            <h2>144</h2>
+            <p>Followers</p>
+          </div>
+          <div className={s.user_raiting}>
+            <h2>100</h2>
+            <p>Following</p>
+          </div>
+        </div>
+        <div className={s.user_info_bottom}>
           <div className={s.user_title}>
             <p className={s.user_title_name}>Sarthak Ranjan Hota</p>
             <p className={s.user_title_bio}>
@@ -51,7 +53,9 @@ const ProfilePage: React.FC = () => {
           {recipes.length === 0 ? (
             <div></div>
           ) : (
-            recipes.slice(0, 8).map((recipe) => <RecipeCard recipe={recipe} />)
+            recipes
+              .slice(0, isSmallScreen ? 4 : 8)
+              .map((recipe) => <RecipeCard recipe={recipe} />)
           )}
         </div>
       </div>
